81c048c6e92295c25b7c7fc79f2ec75c0d8884c1
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-03-02 16:33:14 +0200

    Refactor week2-ships-4.sql query

--- exercises/2/ships/4.sql
+++ exercises/2/ships/4.sql
@@ -1,33 +1,13 @@
 SELECT DISTINCT
-    t1.ship
-FROM (
-    SELECT
-        t.m2,
-        out1.ship,
-        out1.result
-    FROM (
-        SELECT
-            battles.name AS m1,
-            battles.date AS m2
-        FROM
-            battles
-        ORDER BY
-            date offset 0 ROWS) AS t
-        INNER JOIN outcomes out1 ON out1.battle = t.m1) AS t1
-    INNER JOIN (
-        SELECT
-            t.m2,
-            out1.ship,
-            out1.result
-        FROM (
-            SELECT
-                battles.name AS m1,
-                battles.date AS m2
-            FROM
-                battles
-            ORDER BY
-                date offset 0 ROWS) AS t
-            INNER JOIN outcomes out1 ON out1.battle = t.m1) AS t2 ON t1.result != t2.result
+    o1.ship
+FROM
+    outcomes AS o1,
+    battles AS b1,
+    outcomes AS o2,
+    battles AS b2
 WHERE
-    t1.ship = t2.ship
-    AND t1.m2 < t2.m2;
+    o1.battle = b1.name
+    AND o2.battle = b2.name
+    AND o1.ship = o2.ship
+    AND o1.result = 'damaged'
+    AND b1.date < b2.date;
